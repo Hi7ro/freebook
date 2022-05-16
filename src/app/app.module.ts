98a03edc54b9e020d489edb33a833e40760a8db0
xv7ro <xv7ro@xyz.local>
2022-05-16 11:44:03 +0200

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FreeCardComponent } from './free-card/free-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FreeCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/free-card/free-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-free-card',
+  templateUrl: './free-card.component.html',
+  styleUrls: ['./free-card.component.scss']
+})
+export class FreeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
